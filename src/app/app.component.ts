a24703f04afe0a48b583097d5df6f96adad3d20b
Marcin Duda <geotix.md@gmail.com>
2022-07-25 00:36:15 +0200

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Lista Zadań:';
+  tasks: Task[] = [{
+    name: 'pływanie',
+    deadline: '2022-02-01',
+    done: false
+  }];
+  getFooter(): string {
+    return "2022 lista zadań"
+  }
+  get getAdditionalInfo(): string {
+    return 'Autor: Marcin Duda';
+  }
+  get getDate(): Date {
+    return new Date();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import localePl from '@angular/common/locales/pl'
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePl);
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pl-PL'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
